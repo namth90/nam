af7b86c8046950029656ba646c7336298f76020b
namth90 <namth90@yahoo.com>
2012-11-30 04:57:05 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nam::Application.config.secret_token = '74cbea3116c2a77a4b76ab375fed4a097a3c62b344ff743c4c60dda95805c0f171a1a56cd181e7062c5333d6690934e3084160c2597fa9dd24d4d58ca512bc71'
